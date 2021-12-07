16580ae7ed0a7f0f150a8293894e9e93f839dd4e
Alvin Philips <alvinphilips257@gmail.com>
2021-12-08 01:57:56 +0530

    Added Circle collider

--- moon/src/collider.rs
+++ moon/src/collider.rs
@@ -23,10 +23,34 @@ impl AABB {
         }
     }
 }
+#[derive(Debug, Default)]
+pub struct Circle {
+    pub origin: Vector2<f32>,
+    pub radius: f32,
+}
+
+impl Circle {
+    pub fn new_position(x: f32, y: f32) -> Self {
+        Self {
+            origin: Vector2::new(x, y),
+            radius: 1.0,
+        }
+    }
+}
+
 impl Collider for AABB {
     fn get_bounding_box(&self) -> AABB {
         AABB { 
             ..*self
+        }
+    }
+}
+
+impl Collider for Circle {
+    fn get_bounding_box(&self) -> AABB {
+        AABB { 
+            min: self.origin - Vector2::from_element(1.0),
+            max: self.origin + Vector2::from_element(1.0),
         }
     }
 }
@@ -37,3 +61,23 @@ impl Collide<AABB> for AABB {
             self.min.y < _other.max.y && _other.min.y < self.max.y
     }
 }
+
+impl Collide<Circle> for AABB {
+    fn collide_with(&self, _other: &Circle) -> bool {
+        todo!()
+    }
+}
+
+impl Collide<Circle> for Circle {
+    fn collide_with(&self, _other: &Circle) -> bool {
+        let distance: Vector2<f32> = _other.origin - self.origin;
+        distance.norm_squared() < self.radius * _other.radius
+    }
+}
+
+impl Collide<AABB> for Circle {
+    fn collide_with(&self, _other: &AABB) -> bool {
+        _other.collide_with(self)
+    }
+}
+
