46dbae4fc75c2ebc349601e26f392c2973ca6f2d
Alvin Philips <alvinphilips257@gmail.com>
2021-12-08 16:40:01 +0530

    Switched to using Point as an internal Vector2<f32>

--- moon/src/collider.rs
+++ moon/src/collider.rs
@@ -1,4 +1,4 @@
-use nalgebra::Vector2;
+use nalgebra::{Vector2, clamp};
 
 const POINT_BOUNDING_SIZE: f32 = 0.1;
 
@@ -10,44 +10,32 @@ pub trait Collide<T: Collider> {
     fn collide_with(&self, _other: &T) -> bool;
 }
 
-#[derive(Debug, Default)]
-// Point
-pub struct Point {
-    pub coords: Vector2<f32>,
-}
-
-impl Point {
-    pub fn new_position(x: f32, y: f32) -> Self {
-        Self {
-            coords: Vector2::new(x, y),
-        }
-    }
-}
+pub type Point = Vector2<f32>;
 
 // Axis-Aligned Bounding Box
 #[derive(Debug, Default)]
 pub struct AABB {
-    pub min: Vector2<f32>,
-    pub max: Vector2<f32>,
+    pub min: Point,
+    pub max: Point,
 }
 
 impl AABB {
     pub fn new_position(x: f32, y: f32) -> Self {
         Self {
-            min: Vector2::new(x - 0.5, y - 0.5),
-            max: Vector2::new(x + 0.5, y + 0.5),
+            min: Point::new(x - 0.5, y - 0.5),
+            max: Point::new(x + 0.5, y + 0.5),
         }
     }
     pub fn new_size(width: f32, height: f32) -> Self {
-        let half: Vector2<f32> = Vector2::new(width / 2.0, height / 2.0);
+        let half: Point = Point::new(width / 2.0, height / 2.0);
         Self {
             min: -half,
             max: half,
         }
     }
     pub fn new_position_and_size(x: f32, y: f32, width: f32, height: f32) -> Self {
-        let position: Vector2<f32> = Vector2::new(x, y);
-        let half: Vector2<f32> = Vector2::new(width / 2.0, height / 2.0);
+        let position: Point = Point::new(x, y);
+        let half: Point = Point::new(width / 2.0, height / 2.0);
         Self {
             min: position - half,
             max: position + half,
@@ -56,26 +44,26 @@ impl AABB {
 }
 #[derive(Debug, Default)]
 pub struct Circle {
-    pub origin: Vector2<f32>,
+    pub origin: Point,
     pub radius: f32,
 }
 
 impl Circle {
     pub fn new_position(x: f32, y: f32) -> Self {
         Self {
-            origin: Vector2::new(x, y),
+            origin: Point::new(x, y),
             radius: 1.0,
         }
     }
     pub fn new_size(radius: f32) -> Self {
         Self {
-            origin: Vector2::zeros(),
+            origin: Point::zeros(),
             radius
         }
     }
     pub fn new_position_and_size(x: f32, y: f32, radius: f32) -> Self {
         Self {
-            origin: Vector2::new(x, y),
+            origin: Point::new(x, y),
             radius
         }
     }
@@ -83,7 +71,7 @@ impl Circle {
 
 impl Collider for Point {
     fn get_bounding_box(&self) -> AABB {
-        AABB::new_position_and_size(self.coords.x, self.coords.y, POINT_BOUNDING_SIZE, POINT_BOUNDING_SIZE)
+        AABB::new_position_and_size(self.x, self.y, POINT_BOUNDING_SIZE, POINT_BOUNDING_SIZE)
     }
 }
 
@@ -98,15 +86,15 @@ impl Collider for AABB {
 impl Collider for Circle {
     fn get_bounding_box(&self) -> AABB {
         AABB { 
-            min: self.origin - Vector2::from_element(1.0),
-            max: self.origin + Vector2::from_element(1.0),
+            min: self.origin - Point::from_element(1.0),
+            max: self.origin + Point::from_element(1.0),
         }
     }
 }
 
 impl Collide<Point> for Point {
     fn collide_with(&self, _other: &Point) -> bool {
-        self.coords == _other.coords
+        self == _other
     }
 }
 
@@ -131,27 +119,31 @@ impl Collide<AABB> for AABB {
 
 impl Collide<Point> for AABB {
     fn collide_with(&self, _other: &Point) -> bool {
-        self.min.x < _other.coords.x && _other.coords.x < self.max.x &&
-            self.min.y < _other.coords.y && _other.coords.y < self.max.y
+        self.min.x < _other.x && _other.x < self.max.x &&
+            self.min.y < _other.y && _other.y < self.max.y
     }
 }
 
 impl Collide<Circle> for AABB {
     fn collide_with(&self, _other: &Circle) -> bool {
-        todo!()
+        let mut closest: Point = _other.origin;
+        closest.x = clamp(closest.x, self.min.x, self.max.x);
+        closest.y = clamp(closest.y, self.min.y, self.max.y);
+        
+        _other.collide_with(&closest)
     }
 }
 
 impl Collide<Circle> for Circle {
     fn collide_with(&self, _other: &Circle) -> bool {
-        let distance: Vector2<f32> = _other.origin - self.origin;
+        let distance: Point = _other.origin - self.origin;
         distance.norm_squared() < self.radius * _other.radius
     }
 }
 
 impl Collide<Point> for Circle {
     fn collide_with(&self, _other: &Point) -> bool {
-        let distance: Vector2<f32> = _other.coords - self.origin;
+        let distance: Point = _other - self.origin;
         distance.norm_squared() < self.radius * self.radius
     }
 }
