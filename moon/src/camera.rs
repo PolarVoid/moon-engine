f4cb7247650353b62ddbe37788b01680a53d8e3c
Alvin Philips <alvinphilips257@gmail.com>
2021-12-10 13:23:42 +0530

    Implemented default trait for Camera

--- moon/src/camera.rs
+++ moon/src/camera.rs
@@ -1,9 +1,13 @@
 use crate::transform::Transform;
-use nalgebra::{Vector3, Orthographic3};
+use nalgebra::Orthographic3;
+use nalgebra::Vector3;
 
-const FIXED_WIDTH: f32 = 5.0;
-const HEIGHT: f32 = FIXED_WIDTH/1.77;
+/// The 'X' component at the left and right edges of the screen
+const FIXED_WIDTH: f32 = 5.0; 
+/// Calculate the height from the `FIXED_WIDTH` to maintain 16:9 Aspect ratio
+const HEIGHT: f32 = FIXED_WIDTH/1.77; 
 
+/// A `Camera` represents a Virtual Camera, that has a view and Orthographic projection matrices
 #[allow(dead_code)]
 pub struct Camera {
     pub transform: Transform,
@@ -18,27 +22,20 @@ pub struct Camera {
 impl Camera {
     /// Create a new `Camera` with default values.
     pub fn new() -> Self {
-        Self {
-            transform: Transform::new(),
-            width: 1920.0,
-            height: 1080.0,
-            znear: 0.0f32,
-            zfar: 1000.0f32,
-            orthographic: Orthographic3::new(-FIXED_WIDTH, FIXED_WIDTH, -HEIGHT, HEIGHT, 0f32, 1000.0f32),
-        }
+        Default::default()
     }
     /// Create a new `Camera` with an initial position.
     pub fn with_position(position: Vector3<f32>) -> Self {
         Self {
             transform: Transform::new_with_position(position),
-            ..Camera::new()
+            ..Default::default()
         }
     }
     /// Create a new `Camera` with an initial position.
     pub fn with_transform(transform: Transform) -> Self {
         Self {
             transform,
-            ..Camera::new()
+            ..Default::default()
         }
     }
     /// Create a new `Camera` with an initial width and height.
@@ -47,7 +44,7 @@ impl Camera {
             width,
             height,
             //orthographic: Orthographic3::new(0.0, width, 0.0, height, 0f32, 1000.0f32),
-            ..Camera::new()
+            ..Default::default()
         }
     }
     /// Set the width and height of the camera plane, and update the Projection Matrix to match.
@@ -60,5 +57,18 @@ impl Camera {
     /// Return the Projection Matrix of the `Camera` as a slice of `f32` so it can be used by WebGL.
     pub fn projection(&self) -> &[f32] {
         self.orthographic.as_matrix().as_slice()
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Self {
+            transform: Transform::new(),
+            width: 1920.0,
+            height: 1080.0,
+            znear: 0.0f32,
+            zfar: 1000.0f32,
+            orthographic: Orthographic3::new(-FIXED_WIDTH, FIXED_WIDTH, -HEIGHT, HEIGHT, 0f32, 1000.0f32),
+        }
     }
 }
