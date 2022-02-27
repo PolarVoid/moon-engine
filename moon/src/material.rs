91d5756c72475d78498b1d9b0489c3c462455e2d
Alvin Philips <alvinphilips257@gmail.com>
2022-02-27 17:34:15 +0530

    Implemented Default trait for Material

--- moon/src/material.rs
+++ moon/src/material.rs
@@ -1,5 +1,20 @@
+use std::collections::BTreeMap;
+use web_sys::WebGlUniformLocation;
+
 use crate::shader;
 
 pub struct Material {
     shader: shader::Shader,
+}
+
+impl Default for Material {
+    fn default() -> Self {
+        Self { 
+            shader: Default::default()
+        }
+    }
+}
+
+impl Material {
+
 }
