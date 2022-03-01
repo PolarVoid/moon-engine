5fccd35c90f23376fa884a76af0fb405cbdc5760
Alvin Philips <alvinphilips257@gmail.com>
2022-03-01 14:45:16 +0530

    Updated Camera Orthographic matrix

--- moon/src/camera.rs
+++ moon/src/camera.rs
@@ -3,7 +3,7 @@ use nalgebra::Orthographic3;
 use nalgebra::Vector2;
 
 /// The 'X' component at the left and right edges of the screen
-const FIXED_WIDTH: f32 = 5.0;
+const FIXED_WIDTH: f32 = 20.0;
 /// Calculate the height from the `FIXED_WIDTH` to maintain 16:9 Aspect ratio
 const HEIGHT: f32 = FIXED_WIDTH / 1.77;
 
@@ -31,7 +31,7 @@ impl Camera {
             ..Default::default()
         }
     }
-    /// Create a new `Camera` with an initial position.
+    /// Create a new `Camera` with an initial transform.
     pub fn with_transform(transform: Transform) -> Self {
         Self {
             transform,
@@ -43,7 +43,7 @@ impl Camera {
         Self {
             width,
             height,
-            //orthographic: Orthographic3::new(0.0, width, 0.0, height, 0f32, 1000.0f32),
+            orthographic: Orthographic3::new(-width/2.0, width/2.0, height/2.0, -height/2.0, 0f32, 1000.0f32),
             ..Default::default()
         }
     }
@@ -51,8 +51,6 @@ impl Camera {
     pub fn set_width_and_height(&mut self, width: f32, height: f32) {
         self.width = width;
         self.height = height;
-        // self.orthographic.set_right(width);
-        // self.orthographic.set_top(height);
     }
     /// Return the Projection Matrix of the `Camera` as a slice of `f32` so it can be used by WebGL.
     pub fn projection(&self) -> &[f32] {
@@ -76,10 +74,10 @@ impl Default for Camera {
             znear: 0.0f32,
             zfar: 1000.0f32,
             orthographic: Orthographic3::new(
-                -FIXED_WIDTH,
-                FIXED_WIDTH,
-                -HEIGHT,
-                HEIGHT,
+                -FIXED_WIDTH/2.0,
+                FIXED_WIDTH/2.0,
+                HEIGHT/2.0,
+                -HEIGHT/2.0,
                 0f32,
                 1000.0f32,
             ),
