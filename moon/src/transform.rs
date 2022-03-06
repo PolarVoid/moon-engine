839671e30f4e3b601eb0201d125bbf3cf858fc6e
Alvin Philips <alvinphilips257@gmail.com>
2022-03-06 14:36:59 +0530

    Filled in missing functionality in Transform

--- moon/src/transform.rs
+++ moon/src/transform.rs
@@ -1,8 +1,8 @@
-use nalgebra::Matrix4;
+use nalgebra::Matrix3;
 use nalgebra::Vector2;
 
 pub struct Transform {
-    matrix: Matrix4<f32>,
+    matrix: Matrix3<f32>,
     pub position: Vector2<f32>,
     pub rotation: f32,
     pub scale: Vector2<f32>,
@@ -11,7 +11,7 @@ pub struct Transform {
 impl Default for Transform {
     fn default() -> Self {
         Self {
-            matrix: Matrix4::identity(),
+            matrix: Matrix3::identity(),
             position: Vector2::zeros(),
             rotation: 0.0f32,
             scale: Vector2::from_element(1.0),
@@ -28,7 +28,7 @@ impl Transform {
     pub fn new_with_position(position: Vector2<f32>) -> Self {
         Self {
             position,
-            matrix: Matrix4::identity(),
+            matrix: Matrix3::new_translation(&position),
             ..Self::default()
         }
     }
@@ -37,9 +37,34 @@ impl Transform {
         self.matrix.as_slice()
     }
 
+    fn recalculate_matrix(&mut self) {
+        self.matrix = Matrix3::new_translation(&self.position) * Matrix3::new_rotation(self.rotation) * Matrix3::new_nonuniform_scaling(&self.scale);
+    }
+
+    /// Set the positon of the Transform
+    pub fn set_position(&mut self, position: Vector2<f32>) {
+        self.position = position;
+        self.recalculate_matrix();
+    }
+
+    pub fn set_rotation(&mut self, rotation: f32) {
+        self.rotation = rotation;
+        self.recalculate_matrix();
+    }
+
+    pub fn set_scale(&mut self, scale: Vector2<f32>) {
+        self.scale = scale;
+        self.recalculate_matrix();
+    }
+
     /// Get the position as a slice
     pub fn get_position(&self) -> &[f32] {
         self.position.as_slice()
+    }
+
+    /// Get the rotation as a f32
+    pub fn get_rotation(&self) -> f32 {
+        self.rotation
     }
 
     /// Get the scale as a slice
