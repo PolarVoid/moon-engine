330fa094df7dcd33e036ecbf6ede36ef13396d54
Alvin Philips <alvinphilips257@gmail.com>
2022-03-14 13:53:36 +0530

    Added ability to create textures from pixel data

--- moon/src/texture.rs
+++ moon/src/texture.rs
@@ -5,6 +5,7 @@ use web_sys::HtmlImageElement;
 use web_sys::WebGlTexture;
 
 use crate::gl::Bind;
+use crate::Color32;
 use crate::{gl, GL};
 
 #[derive(Debug)]
@@ -71,7 +72,7 @@ impl Texture {
             width,
             height,
             texture,
-            _slot: 0,
+            ..Default::default()
         }
     }
     pub fn new_with_element_id(gl: &GL, image_src: &str) -> Self {
@@ -87,6 +88,60 @@ impl Texture {
     /// Create a new `Texture` from an `HtmlImageElement` with an element ID in the format **textureXX** where *XX* is a number
     pub fn new_with_texture_id(gl: &GL, count: u32) -> Self {
         Self::new_with_element_id(gl, &format!("texture{}", count))
+    }
+    pub fn new_from_pixels(gl: &GL, width: u32, height: u32, pixels: &[u8]) -> Self {
+        assert!(pixels.len() == (width * height * 4) as usize);
+        let texture = gl.create_texture();
+        gl.active_texture(GL::TEXTURE0);
+        gl.bind_texture(GL::TEXTURE_2D, texture.as_ref());
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_S, GL::REPEAT as i32);
+        gl.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_T, GL::REPEAT as i32);
+        gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+            GL::TEXTURE_2D,
+            0,
+            GL::RGBA as i32,
+            width as i32,
+            height as i32,
+            0,
+            GL::RGBA,
+            GL::UNSIGNED_BYTE,
+            Some(pixels),
+        )
+        .expect("Failed to generate texture");
+        gl.generate_mipmap(GL::TEXTURE_2D);
+        Self {
+            width,
+            height,
+            texture,
+            ..Default::default()
+        }
+    }
+    pub fn colored(gl: &GL, color: Color32) -> Self {
+        Self::new_from_pixels(gl, 1, 1, &<[u8; 4]>::from(color))
+    }
+    pub fn white(gl: &GL) -> Self {
+        Self::colored(gl, crate::WHITE)
+    }
+    pub fn checkerboard(gl: &GL) -> Self {
+        Self::checkerboard_colored(gl, crate::WHITE, crate::BLACK)
+    }
+    pub fn checkerboard_colored(gl: &GL, color1: Color32, color2: Color32) -> Self {
+        let size = 8;
+        let mut pixels = Vec::<u8>::with_capacity(size * size);
+        for x_offset in 0..size {
+            for y_offset in 0..size {
+                let color: Color32;
+                if (x_offset + y_offset) % 2 == 0 {
+                    color = color1;
+                } else {
+                    color = color2;
+                }
+                pixels.append(&mut Vec::from(color));
+            }
+        }
+        Self::new_from_pixels(gl, size as u32, size as u32, &pixels)
     }
 }
 
@@ -114,11 +169,11 @@ impl SubTexture {
             ..Default::default()
         }
     }
-    pub fn new_with_coords(texture: Rc<Texture>, uv: [f32; 4]) -> Self {
+    pub fn new_with_coords(texture: Rc<Texture>, uv: Color32) -> Self {
         Self {
             texture: Some(texture),
-            min: [uv[0], uv[2]],
-            max: [uv[1], uv[3]],
+            min: [uv.x(), uv.z()],
+            max: [uv.y(), uv.w()],
         }
     }
     pub fn get_uv_coords(&self) -> [[f32; 2]; 4] {
