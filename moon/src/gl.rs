6f168c3598ef6fb3fdfb9863c58b1904d8889aed
Alvin Philips <alvinphilips257@gmail.com>
2022-03-23 18:28:22 +0530

    Moved renderer to renderer mod

--- moon/src/gl.rs
+++ moon/src/gl.rs
@@ -1,20 +1,43 @@
-use std::{collections::BTreeMap, rc::Rc};
+//! A collection of functions and traits related to [`WebGl2RenderingContext`], as well as the [`GL`] alias.
 
-use crate::{
-    mesh::{Vertex, MAX_BATCH_INDICES, MAX_BATCH_VERTICES},
-    texture::SubTexture,
-    Camera, Canvas, Mesh, Shader, Texture, Transform, Vec2, Color32,
-};
 use wasm_bindgen::JsCast;
-use web_sys::{WebGl2RenderingContext, WebGlUniformLocation};
+use web_sys::WebGl2RenderingContext;
 
+use crate::Canvas;
+
+/// An alias for [`WebGl2RenderingContext`].
+/// 
+/// # Examples
+/// 
+/// ```no_run
+/// use moon::*;
+/// let gl = gl::get_context();
+/// ```
 pub type GL = WebGl2RenderingContext;
 
+/// The [`Bind`] trait enables setting up WebGl state when bound, and optionally resetting to a neutral one when unbound.
+/// 
+/// These functons should be implented **without** mutating the implementing struct, i.e. just changing the state of the [`WebGl2RenderingContext`].
 pub trait Bind {
+    /// Binds a struct implementing the [`Bind`] trait. This is up to the implementation to decide.
     fn bind(&self, gl: &GL);
+    /// Optionally unbinds a struct implementing the [`Bind`] trait. This effectively resets the WebGL state.
     fn unbind(&self, _gl: &GL) {}
 }
 
+/// Check for, and print any WebGL errors if found.
+/// 
+/// Takes a reference to a [`WebGl2RenderingContext`] and returns a [`bool`], indicating whether any errors were found.
+/// 
+/// # Examples
+/// 
+/// ```no_run
+/// use moon::*;
+/// # let gl = gl::get_context();
+/// 
+/// let has_errors = gl::check_gl_error(&gl);
+/// ```
+/// 
 pub fn check_gl_error(gl: &GL) -> bool {
     let mut found_error = false;
     let mut gl_error = gl.get_error();
@@ -26,6 +49,17 @@ pub fn check_gl_error(gl: &GL) -> bool {
     found_error
 }
 
+/// Get the `WebGl2RenderingContext` of a canvas with an *element ID* of **"canvas"**
+/// 
+/// This function will panic in case of an error
+/// 
+/// # Examples
+/// 
+/// ```no_run
+/// use moon::gl::*;
+/// 
+/// let context: GL = get_context();
+/// ```
 pub fn get_context() -> GL {
     let document: web_sys::Document = web_sys::window().unwrap().document().unwrap();
     let canvas: Canvas = document
@@ -40,272 +74,4 @@ pub fn get_context() -> GL {
         .dyn_into::<GL>()
         .unwrap();
     context
-}
-
-#[derive(Debug)]
-pub struct Renderer {
-    pub gl: GL,
-    pub program: Shader,
-    pub camera: Camera,
-    batches: Vec<Mesh>,
-    textures: BTreeMap<&'static str, Rc<Texture>>,
-    u_time: Option<WebGlUniformLocation>,
-    u_color: Option<WebGlUniformLocation>,
-    u_model_matrix: Option<WebGlUniformLocation>,
-    u_view_matrix: Option<WebGlUniformLocation>,
-    u_projection_matrix: Option<WebGlUniformLocation>,
-}
-
-impl Default for Renderer {
-    fn default() -> Self {
-        let gl = get_context();
-        let program = Shader::new(&gl);
-        Self {
-            camera: Camera::default(),
-            batches: Vec::new(),
-            u_time: program.get_uniform_location(&gl, "uTime"),
-            u_color: program.get_uniform_location(&gl, "uColor"),
-            u_model_matrix: program.get_uniform_location(&gl, "uModel"),
-            u_view_matrix: program.get_uniform_location(&gl, "uView"),
-            u_projection_matrix: program.get_uniform_location(&gl, "uProj"),
-            program,
-            textures: {
-                let mut textues = BTreeMap::<&str, Rc<Texture>>::new();
-                textues.insert("WHITE", Rc::new(Texture::white(&gl)));
-                textues.insert("MAGENTA", Rc::new(Texture::colored(&gl, crate::MAGENTA)));
-                textues.insert("CHECKERBOARD", Rc::new(Texture::checkerboard(&gl)));
-                textues
-            },
-            gl,
-        }
-    }
-}
-
-#[derive(Default, Debug, Clone, Copy)]
-pub struct QuadVertex {
-    pub position: Vec2,
-    pub color: Color32
-}
-
-impl QuadVertex {
-    pub fn new(x: f32, y: f32) -> Self {
-        Self {
-            position: Vec2::new(x, y),
-            color: Color32(0.5, 0.4, 0.2, 1.0)
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Quad {
-    pub points: [QuadVertex; 4],
-    pub sprite: SubTexture,
-}
-
-impl Default for Quad {
-    fn default() -> Self {
-        Self {
-            points: [
-                QuadVertex::new(-0.5, 0.5),
-                QuadVertex::new(-0.5, -0.5),
-                QuadVertex::new(0.5, -0.5),
-                QuadVertex::new(0.5, 0.5),
-            ],
-            sprite: SubTexture::default(),
-        }
-    }
-}
-
-impl Quad {
-    pub fn new_from_transform(transform: Transform) -> Self {
-        let origin: [f32; 2] = [transform.position.x, transform.position.y];
-        let offset: [f32; 2] = [transform.scale.x / 2.0, transform.scale.y / 2.0];
-        Self {
-            points: [
-                QuadVertex::new(origin[0] - offset[0], origin[1] + offset[1]),
-                QuadVertex::new(origin[0] - offset[0], origin[1] - offset[1]),
-                QuadVertex::new(origin[0] + offset[0], origin[1] - offset[1]),
-                QuadVertex::new(origin[0] + offset[0], origin[1] + offset[1]),
-            ],
-            ..Default::default()
-        }
-    }
-    pub fn get_vertices(&self) -> Vec<Vertex> {
-        let vertices = std::iter::zip(self.points, self.sprite.get_uv_coords());
-        vertices
-            .map(|(vertex, uv)| Vertex {
-                position: [vertex.position.x, vertex.position.y],
-                uv,
-                color: <[f32; 4]>::from(vertex.color)
-            })
-            .collect()
-    }
-}
-
-impl Renderer {
-    pub fn new_with_camera(camera: Camera) -> Self {
-        Self {
-            camera,
-            ..Default::default()
-        }
-    }
-    pub fn new_with_camera_and_program(camera: Camera, program: Shader) -> Self {
-        let gl = get_context();
-        program.bind(&gl);
-
-        Self {
-            camera,
-            u_time: program.get_uniform_location(&gl, "uTime"),
-            u_color: program.get_uniform_location(&gl, "uColor"),
-            u_model_matrix: program.get_uniform_location(&gl, "uModel"),
-            u_view_matrix: program.get_uniform_location(&gl, "uView"),
-            u_projection_matrix: program.get_uniform_location(&gl, "uProj"),
-            program,
-            gl,
-            ..Default::default()
-        }
-    }
-    pub fn set_camera(&mut self, camera: Camera) {
-        self.camera = camera;
-    }
-    pub fn set_shader(&mut self, program: Shader) {
-        let gl = &self.gl;
-
-        self.u_time = program.get_uniform_location(gl, "uTime");
-        self.u_color = program.get_uniform_location(gl, "uColor");
-        self.u_model_matrix = program.get_uniform_location(gl, "uModel");
-        self.u_view_matrix = program.get_uniform_location(gl, "uView");
-        self.u_projection_matrix = program.get_uniform_location(gl, "uProj");
-        self.program = program;
-    }
-    pub fn resize(&mut self, width: f32, height: f32) {
-        self.camera.set_width_and_height(width, height);
-        self.gl.viewport(0, 0, width as i32, height as i32);
-        self.gl.uniform_matrix4fv_with_f32_array(
-            self.u_projection_matrix.as_ref(),
-            false,
-            self.camera.projection(),
-        );
-    }
-    pub fn init_shader(&mut self) {
-        let gl = &self.gl;
-        self.program.bind(gl);
-
-        gl.uniform1f(self.u_time.as_ref(), 0.0);
-        gl.uniform4f(self.u_color.as_ref(), 1.0, 1.0, 1.0, 1.0);
-        gl.uniform_matrix4fv_with_f32_array(
-            self.u_view_matrix.as_ref(),
-            false,
-            crate::Mat4::identity().as_slice(),
-        );
-        gl.uniform_matrix4fv_with_f32_array(
-            self.u_view_matrix.as_ref(),
-            false,
-            self.camera.transform.matrix(),
-        );
-        gl.uniform_matrix4fv_with_f32_array(
-            self.u_projection_matrix.as_ref(),
-            false,
-            self.camera.projection(),
-        );
-    }
-    pub fn add_texture(&mut self, key: &'static str, texture: Texture) {
-        self.textures.insert(key, Rc::new(texture));
-    }
-    pub fn use_texture(&self, key: &str) {
-        let gl = &self.gl;
-        if let Some(texture) = self.textures.get(key) {
-            texture.bind(gl);
-        } else {
-            self.textures.get("MAGENTA").unwrap().bind(gl);
-        }
-    }
-    pub fn get_texture(&mut self, key: &str) -> Rc<Texture> {
-        Rc::clone(
-            self.textures
-                .get(key)
-                .unwrap_or_else(|| self.textures.get("MAGENTA").unwrap()),
-        )
-    }
-    pub fn begin_draw(&mut self) {
-        let gl = &self.gl;
-
-        self.batches.clear();
-
-        let mesh = Mesh::new(
-            gl,
-            Vec::with_capacity(MAX_BATCH_VERTICES as usize),
-            Vec::with_capacity(MAX_BATCH_INDICES as usize),
-        );
-
-        self.batches.push(mesh);
-    }
-    pub fn add_quad(&mut self, quad: Quad) {
-        let gl = &self.gl;
-
-        // Get last batch. This should never be empty becase begin_draw should have been called before.
-        let mut batch = self
-            .batches
-            .last_mut()
-            .expect("Batch list empty. Check if begin_draw was called before.");
-        if batch.vertices.len() + 4 > MAX_BATCH_VERTICES as usize {
-            let mesh = Mesh::new(gl, Vec::new(), Vec::new());
-            self.batches.push(mesh);
-
-            batch = self.batches.last_mut().unwrap();
-        }
-
-        let last = batch.vertices.len() as u32;
-        batch.vertices.append(&mut quad.get_vertices());
-        let mut indices = vec![last, last + 2, last + 1, last, last + 3, last + 2];
-        batch.indices.append(&mut indices);
-    }
-    pub fn begin_layer(&mut self) {
-        let gl = &self.gl;
-        let mesh = Mesh::new(
-            gl,
-            Vec::with_capacity(MAX_BATCH_VERTICES as usize),
-            Vec::with_capacity(MAX_BATCH_INDICES as usize),
-        );
-
-        self.batches.push(mesh);
-    }
-    pub fn delete_layer(&mut self) -> Option<Mesh> {
-        self.batches.pop()
-    }
-    pub fn draw_layer(&self) {
-        let gl = &self.gl;
-        if let Some(batch) = self.batches.last() {
-            batch.setup(gl);
-            gl.draw_elements_with_i32(
-                GL::TRIANGLES,
-                batch.indices.len() as i32,
-                GL::UNSIGNED_INT,
-                0,
-            );
-        }
-    }
-    pub fn end_draw(&mut self) {
-        let gl = &self.gl;
-        gl.uniform_matrix4fv_with_f32_array(
-            self.u_view_matrix.as_ref(),
-            false,
-            self.camera.transform.matrix(),
-        );
-        self.program.bind(gl);
-        for batch in self.batches.iter() {
-            batch.setup(gl);
-            gl.draw_elements_with_i32(
-                GL::TRIANGLES,
-                batch.indices.len() as i32,
-                GL::UNSIGNED_INT,
-                0,
-            );
-        }
-    }
-    pub fn clear(&mut self, color: [f32; 4]) {
-        let gl = &self.gl;
-        gl.clear_color(color[0], color[1], color[2], color[3]);
-        gl.clear(GL::COLOR_BUFFER_BIT);
-    }
 }
---
+++ moon/src/renderer.rs
@@ -0,0 +1,381 @@
+//! The [`Renderer`] annd [`Quad`] structs. Used for rendering.
+
+use std::collections::BTreeMap;
+use std::rc::Rc;
+use web_sys::WebGlUniformLocation;
+
+use crate::{gl, texture, mesh};
+use crate::{Shader, GL, Camera, Transform};
+
+use gl::Bind;
+use texture::{Texture, SubTexture};
+use mesh::{Vertex, Mesh};
+
+/// Maximum [`Quad`]s in a single batch.
+pub const MAX_BATCH_QUADS: i32 = 1000;
+const MAX_BATCH_VERTICES: i32 = MAX_BATCH_QUADS * 4;
+const MAX_BATCH_INDICES: i32 = MAX_BATCH_QUADS * 6;
+
+/// The [`Renderer`] is responsible for drawing on the screen. It handles the [`Camera`] and [`Shader`]s.
+#[derive(Debug)]
+pub struct Renderer {
+    /// The [`WebGl2RenderingContext`](web_sys::WebGl2RenderingContext) used by the [`Renderer`].
+    pub gl: GL,
+    /// The [`Shader`] used by the [`Renderer`].
+    pub program: Shader,
+    /// The [`Camera`] used by the [`Renderer`].
+    pub camera: Camera,
+    batches: Vec<Mesh>,
+    textures: BTreeMap<&'static str, Rc<Texture>>,
+    u_time: Option<WebGlUniformLocation>,
+    u_color: Option<WebGlUniformLocation>,
+    u_model_matrix: Option<WebGlUniformLocation>,
+    u_view_matrix: Option<WebGlUniformLocation>,
+    u_projection_matrix: Option<WebGlUniformLocation>,
+}
+
+impl Default for Renderer {
+    fn default() -> Self {
+        let gl = gl::get_context();
+        let program = Shader::new(&gl);
+        Self {
+            camera: Camera::default(),
+            batches: Vec::new(),
+            u_time: program.get_uniform_location(&gl, "uTime"),
+            u_color: program.get_uniform_location(&gl, "uColor"),
+            u_model_matrix: program.get_uniform_location(&gl, "uModel"),
+            u_view_matrix: program.get_uniform_location(&gl, "uView"),
+            u_projection_matrix: program.get_uniform_location(&gl, "uProj"),
+            program,
+            textures: {
+                let mut textues = BTreeMap::<&str, Rc<Texture>>::new();
+                textues.insert("WHITE", Rc::new(Texture::white(&gl)));
+                textues.insert("MAGENTA", Rc::new(Texture::colored(&gl, crate::MAGENTA)));
+                textues.insert("CHECKERBOARD", Rc::new(Texture::checkerboard(&gl)));
+                textues
+            },
+            gl,
+        }
+    }
+}
+
+/// A [`Quad`] is a simple mesh definition with four [`Vertices`](Vertex).
+#[derive(Debug)]
+pub struct Quad ([Vertex; 4]);
+
+impl Default for Quad {
+    fn default() -> Self {
+        Self ([
+            Vertex {
+                position: [-0.5, 0.5],
+                uv: [0.0, 0.0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [-0.5, -0.5],
+                uv: [0.0, 1.0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [0.5, -0.5],
+                uv: [1.0, 1.0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [0.5, 0.5],
+                uv: [1.0, 0.0],
+                ..Default::default()
+            }
+        ])
+    }
+}
+
+impl Quad {
+    /// Create a new [`Quad`] from a given position and size.
+    pub fn new_from_position_and_size(pos_x: f32, pos_y: f32, size_x: f32, size_y: f32) -> Self {
+        let size_x = size_x / 2.0;
+        let size_y = size_y / 2.0;
+        Self ([
+            Vertex {
+                position: [pos_x - size_x, pos_y + size_y],
+                uv: [0.0, 0.0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [pos_x - size_x, pos_y - size_y],
+                uv: [0.0, 1.0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [pos_x + size_x, pos_y - size_y],
+                uv: [1.0, 1.0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [pos_x + size_x, pos_y + size_y],
+                uv: [1.0, 0.0],
+                ..Default::default()
+            },
+        ])
+    }
+
+    /// Create a new [`Quad`] from a given position, size, and a reference to a [`SubTexture`].
+    pub fn new_from_position_and_size_and_sprite(pos_x: f32, pos_y: f32, size_x: f32, size_y: f32, sprite: &SubTexture) -> Self {
+        let uv = sprite.get_uv_coords();
+        let size_x = size_x / 2.0;
+        let size_y = size_y / 2.0;
+        Self ([
+            Vertex {
+                position: [pos_x - size_x, pos_y + size_y],
+                uv: uv[0],
+                ..Default::default()
+            },
+            Vertex {
+                position: [pos_x - size_x, pos_y - size_y],
+                uv: uv[1],
+                ..Default::default()
+            },
+            Vertex {
+                position: [pos_x + size_x, pos_y - size_y],
+                uv: uv[2],
+                ..Default::default()
+            },
+            Vertex {
+                position: [pos_x + size_x, pos_y + size_y],
+                uv: uv[3],
+                ..Default::default()
+            },
+        ])
+    }
+
+    /// Create a new [`Quad`] using a given [`Transform`] for its position and scale.
+    pub fn new_from_transform(transform: Transform) -> Self {
+        Self::new_from_position_and_size(
+            transform.position.x, 
+            transform.position.x, 
+            transform.scale.x,
+             transform.scale.y
+        )
+    }
+
+    /// Create a new [`Quad`] using a given [`Transform`] for its position and scale, and a reference to [`SubTexture`].
+    pub fn new_from_transform_and_sprite(transform: Transform, sprite: &SubTexture) -> Self {
+        Self::new_from_position_and_size_and_sprite(
+            transform.position.x, 
+            transform.position.x, 
+            transform.scale.x,
+             transform.scale.y,
+             sprite
+        )
+    }
+
+    /// Get the [`Vertices`](Vertex) of the [`Quad`] as a [`Vec`].
+    pub fn get_vertices(&self) -> Vec<Vertex> {
+        self.0.to_vec()
+    }
+}
+
+impl Renderer {
+    /// Create a new [`Renderer`] with a given [`Camera`].
+    pub fn new_with_camera(camera: Camera) -> Self {
+        Self {
+            camera,
+            ..Default::default()
+        }
+    }
+
+    /// Create a new [`Renderer`] with a given [`Camera`] and [`Shader`].
+    pub fn new_with_camera_and_program(camera: Camera, program: Shader) -> Self {
+        let gl = gl::get_context();
+        program.bind(&gl);
+
+        Self {
+            camera,
+            u_time: program.get_uniform_location(&gl, "uTime"),
+            u_color: program.get_uniform_location(&gl, "uColor"),
+            u_model_matrix: program.get_uniform_location(&gl, "uModel"),
+            u_view_matrix: program.get_uniform_location(&gl, "uView"),
+            u_projection_matrix: program.get_uniform_location(&gl, "uProj"),
+            program,
+            gl,
+            ..Default::default()
+        }
+    }
+
+    /// Set the [`Camera`] that the [`Renderer`] will use.
+    pub fn set_camera(&mut self, camera: Camera) {
+        self.camera = camera;
+    }
+
+    /// Set the [`Shader`] that the [`Renderer`] will use.
+    pub fn set_shader(&mut self, program: Shader) {
+        let gl = &self.gl;
+
+        self.u_time = program.get_uniform_location(gl, "uTime");
+        self.u_color = program.get_uniform_location(gl, "uColor");
+        self.u_model_matrix = program.get_uniform_location(gl, "uModel");
+        self.u_view_matrix = program.get_uniform_location(gl, "uView");
+        self.u_projection_matrix = program.get_uniform_location(gl, "uProj");
+        self.program = program;
+    }
+
+    /// Handle screen resizes.
+    pub fn resize(&mut self, width: f32, height: f32) {
+        self.camera.set_width_and_height(width, height);
+        self.gl.viewport(0, 0, width as i32, height as i32);
+        self.gl.uniform_matrix4fv_with_f32_array(
+            self.u_projection_matrix.as_ref(),
+            false,
+            self.camera.projection(),
+        );
+    }
+
+    /// Initialise the uniforms for the current [`Shader`].
+    pub fn init_shader(&mut self) {
+        let gl = &self.gl;
+        self.program.bind(gl);
+
+        gl.uniform1f(self.u_time.as_ref(), 0.0);
+        gl.uniform4f(self.u_color.as_ref(), 1.0, 1.0, 1.0, 1.0);
+        gl.uniform_matrix4fv_with_f32_array(
+            self.u_view_matrix.as_ref(),
+            false,
+            crate::Mat4::identity().as_slice(),
+        );
+        gl.uniform_matrix4fv_with_f32_array(
+            self.u_view_matrix.as_ref(),
+            false,
+            self.camera.transform.matrix_slice(),
+        );
+        gl.uniform_matrix4fv_with_f32_array(
+            self.u_projection_matrix.as_ref(),
+            false,
+            self.camera.projection(),
+        );
+    }
+
+    /// Add a [`Texture`] to the [`Renderer`].
+    /// 
+    /// The renderer stores [`Texture`]s that can be retreived later, via a string slice.
+    pub fn add_texture(&mut self, key: &'static str, texture: Texture) {
+        self.textures.insert(key, Rc::new(texture));
+    }
+
+    /// Use the requested [`Texture`].
+    /// 
+    /// Sets the currently bound [`Texture`] to the one that matches the key. If no such texture is found, a default MAGENTA one is found.
+    pub fn use_texture(&self, key: &str) {
+        let gl = &self.gl;
+        if let Some(texture) = self.textures.get(key) {
+            texture.bind(gl);
+        } else {
+            self.textures.get("MAGENTA").unwrap().bind(gl);
+        }
+    }
+
+    /// Get the requested [`Texture`], or MAGENTA if none is found.
+    pub fn get_texture(&mut self, key: &str) -> Rc<Texture> {
+        Rc::clone(
+            self.textures
+                .get(key)
+                .unwrap_or_else(|| self.textures.get("MAGENTA").unwrap()),
+        )
+    }
+
+    /// Clear the batch queue and start a new batch.
+    pub fn begin_draw(&mut self) {
+        let gl = &self.gl;
+
+        self.batches.clear();
+
+        let mesh = Mesh::new(
+            gl,
+            Vec::with_capacity(MAX_BATCH_VERTICES as usize),
+            Vec::with_capacity(MAX_BATCH_INDICES as usize),
+        );
+
+        self.batches.push(mesh);
+    }
+
+    /// Add a [`Quad`] to the batching queue.
+    pub fn add_quad(&mut self, quad: Quad) {
+        let gl = &self.gl;
+
+        // Get last batch. This should never be empty becase begin_draw should have been called before.
+        let mut batch = self
+            .batches
+            .last_mut()
+            .expect("Batch list empty. Check if begin_draw was called before.");
+        if batch.vertices.len() + 4 > MAX_BATCH_VERTICES as usize {
+            let mesh = Mesh::new(gl, Vec::new(), Vec::new());
+            self.batches.push(mesh);
+
+            batch = self.batches.last_mut().unwrap();
+        }
+
+        let last = batch.vertices.len() as u32;
+        batch.vertices.append(&mut quad.get_vertices());
+        let mut indices = vec![last, last + 2, last + 1, last, last + 3, last + 2];
+        batch.indices.append(&mut indices);
+    }
+
+    /// Begin a new layer.
+    /// 
+    /// A new mesh is added to the batches and subsequent calls are made on this layer.
+    pub fn begin_layer(&mut self) {
+        let gl = &self.gl;
+        let mesh = Mesh::new(
+            gl,
+            Vec::with_capacity(MAX_BATCH_VERTICES as usize),
+            Vec::with_capacity(MAX_BATCH_INDICES as usize),
+        );
+
+        self.batches.push(mesh);
+    }
+
+    /// Remove the last layer.
+    pub fn delete_layer(&mut self) -> Option<Mesh> {
+        self.batches.pop()
+    }
+
+    /// Draw the current layer.
+    pub fn draw_layer(&self) {
+        let gl = &self.gl;
+        if let Some(batch) = self.batches.last() {
+            batch.setup(gl);
+            gl.draw_elements_with_i32(
+                GL::TRIANGLES,
+                batch.indices.len() as i32,
+                GL::UNSIGNED_INT,
+                0,
+            );
+        }
+    }
+
+    /// Draw all batched geometry.
+    pub fn end_draw(&mut self) {
+        let gl = &self.gl;
+        gl.uniform_matrix4fv_with_f32_array(
+            self.u_view_matrix.as_ref(),
+            false,
+            self.camera.transform.matrix_slice(),
+        );
+        self.program.bind(gl);
+        for batch in self.batches.iter() {
+            batch.setup(gl);
+            gl.draw_elements_with_i32(
+                GL::TRIANGLES,
+                batch.indices.len() as i32,
+                GL::UNSIGNED_INT,
+                0,
+            );
+        }
+    }
+
+    /// Clear the screen with a given Color.
+    pub fn clear(&mut self, color: [f32; 4]) {
+        let gl = &self.gl;
+        gl.clear_color(color[0], color[1], color[2], color[3]);
+        gl.clear(GL::COLOR_BUFFER_BIT);
+    }
+}
