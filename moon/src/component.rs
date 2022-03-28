dbdc19d9d132ee311e5be1930b55bb698f70acbf
Alvin Philips <alvinphilips257@gmail.com>
2022-03-28 16:37:17 +0530

    Updated doc for Component update

--- moon/src/component.rs
+++ moon/src/component.rs
@@ -5,5 +5,7 @@ pub trait Component {
     /// Initializes a [`Component`].
     fn init(&mut self) {}
     /// Called once each frame.
+    /// 
+    /// The time between frames is also provided.
     fn update(&mut self, _delta_time: f32) {}
 }
