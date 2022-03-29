8d71a3b346c715c50b6784faf54a6f3cfbee5718
Alvin Philips <alvinphilips257@gmail.com>
2022-03-29 17:23:54 +0530

    Added use of any trait

--- moon/src/component.rs
+++ moon/src/component.rs
@@ -1,5 +1,9 @@
 //! The [`Component`] trait.
 
+use std::any::Any;
+
+use crate::renderer::Quad;
+
 /// The [`Component`] trait
 pub trait Component {
     /// Initializes a [`Component`].
@@ -8,4 +12,15 @@ pub trait Component {
     ///
     /// The time between frames is also provided.
     fn update(&mut self, _delta_time: f32) {}
+
+    /// Get the [`Component`]'s drawable [`Quad`]
+    fn get_quads(&self) -> Option<Vec<Quad>> {
+        None
+    }
+
+    /// Borrow the [`Component`] as a [`&mut dyn Any`](Any).
+    fn as_any(&self) -> &dyn Any;
+
+    /// Borrow the [`Component`] as a [`&dyn Any`](Any).
+    fn as_mut_any(&mut self) -> &mut dyn Any;
 }
